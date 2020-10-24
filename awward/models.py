21eefaef8192cf7062f557f523dbde5f2e8a7d1d
dancan-sandys <dancan.oruko96@gmail.com>
2020-10-24 12:13:03 +0300

    add the profile, projects and ratings models

--- awward/models.py
+++ awward/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profilepic = models.CharField(max_length=30)
+    bio = models.TextField(max_length=500)
+    Phone = models.IntegerField(max_length=13)
+    email = models.EmailField(max_length=20)
+
+class Project(models.Model):
+    Title = models.CharField(max_length=30)
+    Landing = models.CharField(max_length=30)
+    Description = models.TextField(max_length=500)
+    User = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+class Rating(models.Model):
+    Design = models.IntegerField()
+    Usability = models.IntegerField()
+    Content = models.IntegerField()
+    Average = models.IntegerField()
+    Rater = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
