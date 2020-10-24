c836e23f751dcbddf81ce4f23a15f5cbdfb1e996
dancan-sandys <dancan.oruko96@gmail.com>
2020-10-24 17:02:19 +0300

    add a registration system

--- awward/models.py
+++ awward/models.py
@@ -5,14 +5,32 @@ from django.db import models
 class Profile(models.Model):
     profilepic = models.CharField(max_length=30)
     bio = models.TextField(max_length=500)
-    Phone = models.IntegerField(max_length=13)
+    Phone = models.IntegerField()
     email = models.EmailField(max_length=20)
+
+    def saveprofile(self):
+        self.save()
+
+    def deleteprofile(self):
+        self.delete()
+
+    
+
+
 
 class Project(models.Model):
     Title = models.CharField(max_length=30)
     Landing = models.CharField(max_length=30)
     Description = models.TextField(max_length=500)
     User = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def saveproject(self):
+        self.save()
+
+    def deleteproject(self):
+        self.delete()
+    
+
     
 class Rating(models.Model):
     Design = models.IntegerField()
@@ -21,3 +39,6 @@ class Rating(models.Model):
     Average = models.IntegerField()
     Rater = models.ForeignKey(Profile, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def saverating(self):
+        self.save()
